2bd779587560b0e65c9d1734a7985f7623f241a7
elhlow316 <elhlow986@gmail.com>
2025-09-06 05:57:59 +0300

    Initial platform structure with example frontend and backend files

---
+++ frontend/admin/AdminDashboard.jsx
@@ -0,0 +1,4 @@
+import React from 'react';
+export default function AdminDashboard() {
+  return <div>لوحة المدير</div>;
+}
---
+++ frontend/user/speech/SpeechTranscription.jsx
@@ -0,0 +1,4 @@
+import React from 'react';
+export default function SpeechTranscription() {
+  return <div>صفحة تحويل الكلام لنص</div>;
+}
